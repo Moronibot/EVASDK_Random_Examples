91f49f2f2453262a0126e5eadbf854a080703618
Euan <Euanmeston@gmail.com>
2021-08-10 18:02:53 +0100

    Added another pause example

--- check_joint_angles.py
+++
@@ -1,9 +0,0 @@
-import evasdk
-
-if __name__ == '__main__':
-    IP = '172.16.16.2'
-    TOKEN = '5930e264643dc3d4b4ff0ebb05da0ce404d3281a'
-    robot = evasdk.Eva(IP, TOKEN)
-    print(evasdk.__version__)
-    print(robot.name()['name'])
-    print(robot.versions())
