5991524125f20b93ceb6279346427ec8d7e04a7d
Euan <Euanmeston@gmail.com>
2021-10-13 13:18:17 +0100

    To test

--- run_and_pause.py
+++ run_and_pause.py
@@ -5,37 +5,47 @@ You will need to upload a valid toolpath before running this script.
 """
 import evasdk
 
-# Todo input Eva IP and generate token before use
-IP = None
-TOKEN = None
 
-robot = evasdk.Eva(IP, TOKEN)
+class RunAndPause:
+    def __init__(self, robot_ip, robot_token):
+        self.eva = evasdk.Eva(robot_ip, robot_token)
 
+    def stop_button_monitor(self):
+        """ Monitors the digital input 0 pin for any state change. """
+        while self.eva.data_snapshot()['control']['state'] == 'running':
+            button_pin = self.eva.data_snapshot()['global.inputs']['d0']
+            if button_pin:
+                with self.eva.lock():
+                    self.eva.control_pause()
 
-def stop_button_monitor():
-    """ Monitors the digital input 0 pin for any state change. """
-    while robot.data_snapshot()['control']['state'] == 'running':
-        GLOBAL_FLAG = robot.data_snapshot()['global.inputs']['d0']
-        if GLOBAL_FLAG:
-            with robot.lock():
-                robot.control_pause()
+    def robot_state_handler(self):
+        """ Catches the robot in either state which is suitable to restart the toolpath from. """
+        snapshot = self.eva.data_snapshot()
+        robot_state = snapshot['control']['state']
+        button_active = snapshot['global.inputs']['d0']
+        if robot_state == 'ready' and not button_active:
+            with self.eva.lock():
+                self.eva.control_home()
+                self.eva.control_run(loop=0, wait_for_ready=False, mode='automatic')
+            self.stop_button_monitor()
+        elif robot_state == 'paused' and not button_active:
+            with self.eva.lock():
+                self.eva.control_cancel()
+                self.eva.control_home()
 
-
-def robot_state_handler():
-    """ Catches the robot in either state which is suitable to restart the toolpath from. """
-    ROBOT_STATE = robot.data_snapshot()['control']['state']
-    GLOBAL_FLAG = robot.data_snapshot()['global.inputs']['d0']
-    if ROBOT_STATE == 'ready' and not GLOBAL_FLAG:
-        with robot.lock():
-            robot.control_home()
-            robot.control_run(loop=1, wait_for_ready=False, mode='automatic')
-        stop_button_monitor()
-    elif ROBOT_STATE == 'paused' and not GLOBAL_FLAG:
-        with robot.lock():
-            robot.control_cancel()
-            robot.control_home()
+    def run(self):
+        """ Main running loop """
+        try:
+            while True:
+                self.robot_state_handler()
+        except KeyboardInterrupt:
+            with self.eva.lock():
+                self.eva.control_stop_loop(wait_for_ready=False)
+            quit()
 
 
 if __name__ == '__main__':
-    while True:
-        robot_state_handler()
+    IP = None
+    TOKEN = None
+    app = RunAndPause(IP, TOKEN)
+    app.run()
